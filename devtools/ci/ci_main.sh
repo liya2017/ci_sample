cc5dcccce842a9fce6f659671aabc72bdb8b2329
liya2017 <Liya@cryptape.com>
2021-09-10 13:52:49 +0800

    set env

--- devtools/ci/ci_main.sh
+++ devtools/ci/ci_main.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
 set -u
+is_self_runner=`echo $RUNNER_LABEL | awk -F '-' '{print $1}'`
+if [[ $is_self_runner == "self" ]];then
+    CARGO_TARGET_DIR=$GITHUB_WORKSPACE/../target
+fi
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$GITHUB_WORKSPACE/target"}
 case $GITHUB_WORKFLOW in
 	ci_linters*)
 		echo "Hellow ci_linters!"
+		echo "CARGO_TARGET_DIR is  "$CARGO_TARGET_DIR
 		;;
 	ci_unit_test*)
 		echo "See you unit_test!"
+		echo "CARGO_TARGET_DIR is  "$CARGO_TARGET_DIR
 		;;
 	*)
 		echo -n "unknown"
